8c6db3a051ff81c4e79480732675094caf0b6c6f
16061060 <cs25acb@herts.ac.uk>
2025-12-10 11:08:10 +0000

    add more detail to plots

--- visualisation.R
+++ visualisation.R
@@ -1,24 +1,28 @@
 #install libraries
 library(readr)
+
 #set up variables
 df <- crime_and_unemployment_by_state
 df$Total_Thefts <- df$`property total`
+
 #create histogram
 png("plots/histogram.png")
 hist(df$Total_Thefts,
      main="Thefts Across The United States From 1976 to 2014",
-     xlab="Total Thefts")
+     xlab="Total Thefts",
+     col="lightblue")
 dev.off()
 
 #create plot
 png("plots/scatter.png")
 plot(df$unemployment,
      df$Total_Thefts, 
-     main="Unemployment and Theft Comparision", 
+     main="Unemployment vs Theft Comparision", 
      #sub="Is there a correlation between the amount of thefts and the unemployment rate across the United States between 1976 to 2014?",
-     xlab="Unemployment Rate",
+     xlab="Unemployment Percentage",
      ylab="Total Thefts")
 abline(lm(Total_Thefts ~ unemployment, data = df), col = "red")
 dev.off()
+
 #run test
 cor.test(df$Total_Thefts,df$unemployment, method = "pearson")
