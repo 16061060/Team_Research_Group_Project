adfac737033d8056f149ed40917e0ad751cebec5
23082615 <pd24abb@herts.ac.uk>
2025-12-11 11:43:31 +0000

    Update visualisation.R

--- visualisation.R
+++ visualisation.R
@@ -17,7 +17,7 @@ dev.off()
 png("plots/scatter.png")
 plot(df$unemployment,
      df$Total_Thefts, 
-     main="Unemployment vs Theft Comparision", 
+     main="unemployment vs Theft comparison",
      #sub="Is there a correlation between the amount of thefts and the unemployment rate across the United States between 1976 to 2014?",
      xlab="Unemployment Percentage",
      ylab="Total Thefts")
@@ -25,4 +25,5 @@ abline(lm(Total_Thefts ~ unemployment, data = df), col = "red")
 dev.off()
 
 #run test
+
 cor.test(df$Total_Thefts,df$unemployment, method = "pearson")
